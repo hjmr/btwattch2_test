0ffc21f082516ec79d66fc8c20177fbfadb63665
hjmr <2620990+hjmr@users.noreply.github.com>
2020-11-06 16:40:52 +0900

    enable to set date and time.

--- set_date.py
+++ set_date.py
@@ -13,6 +13,7 @@ CHAR_UART_NOTIFY = ("6e400003-b5a3-f393-e0a9-e50e24dcca9e")
 
 def parse_arg():
     parser = argparse.ArgumentParser(description="Test notification of the specified device.")
+    parser.add_argument("--datetime", type=str, help="Specify date and time in %Y-%m-%d %H:%M:%S format.")
     parser.add_argument("ADDRESS", type=str, help="A device address.")
     return parser.parse_args()
 
@@ -23,23 +24,24 @@ def notification_handler(sender, data):
     print("[Notification] {0}: {1}".format(sender, data_str))
 
 
-async def run(address):
+async def run(address, dt=None):
 
     async with BleakClient(address) as client:
         x = await client.is_connected()
         print("Connected: {0}".format(x))
 
-        await client.start_notify(CHAR_UART_NOTIFY, notification_handler)
+        if dt is None:
+            dt = datetime.now()
 
         set_date_command = bytearray([0xaa, 0x00, 0x08])
         start_data = bytes([0x01])
         end_data = bytes([0x05])
 
-        dt_now = datetime.now()
-        encoded_now = encode_datetime(dt_now.year, dt_now.month, dt_now.day, dt_now.hour, dt_now.minute, dt_now.second)
+        encoded_now = encode_datetime(dt.year, dt.month, dt.day, dt.hour, dt.minute, dt.second)
         data_body = start_data + encoded_now + end_data
-
         write_data = set_date_command + data_body + crc8(data_body)
+
+        await client.start_notify(CHAR_UART_NOTIFY, notification_handler)
 
         await client.write_gatt_char(CHAR_UART_RX, write_data)
         await asyncio.sleep(5.0)
@@ -49,5 +51,9 @@ async def run(address):
 
 if __name__ == "__main__":
     args = parse_arg()
+    dt = None
+    if args.datetime is not None:
+        dt = datetime.strptime(args.datetime, '%Y-%m-%d %H:%M:%S')
+
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(run(args.ADDRESS))
+    loop.run_until_complete(run(args.ADDRESS, dt))
