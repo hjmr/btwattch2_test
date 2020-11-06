97ec6cd96134bcedcbb9a7ffbbeb4dbe5d6389f3
hjmr <2620990+hjmr@users.noreply.github.com>
2020-11-06 13:49:16 +0900

    more testing.

--- utils.py
+++ utils.py
@@ -1,5 +1,12 @@
 from datetime import datetime
 
+import crcmod
+
+
+def crc8(data):
+    crc8_func = crcmod.mkCrcFun(0x185, initCrc=0, rev=False)
+    return crc8_func(data).to_bytes(1, 'big')
+
 
 def decode_data(buffer):
     field_lengths = (
@@ -7,9 +14,9 @@ def decode_data(buffer):
         ('length', 2),  # 1, 2
         ('command', 1),  # 3
         ('error_code', 1),  # 4
-        ('current', 3),  # 5, 6, 7
-        ('voltage', 3),  # 8, 9, 10
-        ('power', 3),  # 11, 12, 13
+        ('current', 6),  # 5, 6, 7
+        ('voltage', 6),  # 8, 9, 10
+        ('power', 6),  # 11, 12, 13
         ('second', 1),  # 14
         ('minute', 1),  # 15
         ('hour', 1),  # 16
@@ -20,7 +27,7 @@ def decode_data(buffer):
     d = {}
     i = 0
     for k, l in field_lengths:
-        v = int.from_bytes(buffer[i:i+l], byteorder='big')
+        v = int.from_bytes(buffer[i:i+l], byteorder='little')
         d[k] = v
         i += l
 
@@ -32,3 +39,16 @@ def decode_data(buffer):
     ret['W'] = d['power'] * 5.0 / 1000.0
 
     return d
+
+
+def encode_datetime(year, month, day, hour, minute, second):
+    return bytearray([second, minute, hour, day, month-1, year-1900])
+
+
+if __name__ == "__main__":
+    command = bytearray([0xaa, 0x00, 0x08, 0x01])
+    encoded_dt = encode_datetime(2020, 9, 26, 1, 35, 00)
+    cmd = command + encoded_dt + bytes([5])
+    cmd += crc8(cmd)
+    data_str = ' '.join(format(x, '02x') for x in cmd)
+    print(data_str)
